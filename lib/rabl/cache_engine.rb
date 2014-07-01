fb160bc1de453c15153546562ad862da9c3c2494
Sosho <soshochang@gmail.com>
2014-07-01 12:36:02 -0700

    add fetch_with_patch method to allow us to use cache in rabl while running rails 2

--- lib/rabl/cache_engine.rb
+++ lib/rabl/cache_engine.rb
@@ -21,5 +21,14 @@ module Rabl
       end
     end
 
+    # We add this fetch_with_patch method to allow us to use cache in rabl while running rails 2 
+    # (rails 3 does not need this b/c it is okay with passing in nil options whereas rails 2 is not)
+    # https://github.com/nesquena/rabl/issues/325
+    def fetch_with_patch(key, cache_options = {}, &block)
+      cache_options ||= {}
+      fetch_without_patch(key, cache_options, &block) #with the alias method chain this calls fetch defined above
+    end
+    alias_method_chain :fetch, :patch
+
   end
 end
